d1c88bd69d72aa1b88b3dcf7d2c40356a33fe20d
kolyn092 <kolyn092@gmail.com>
2026-05-19 18:38:09 +0900

    feat: s3 프로필 이미지 관련 api 추가

--- src/main/java/com/cloudmember/service/MemberService.java
+++ src/main/java/com/cloudmember/service/MemberService.java
@@ -6,14 +6,17 @@ import com.cloudmember.entity.Member;
 import com.cloudmember.repository.MemberRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class MemberService {
 
     private final MemberRepository memberRepository;
+    private final S3Service s3Service;
 
-    public MemberService(MemberRepository memberRepository) {
+    public MemberService(MemberRepository memberRepository, S3Service s3Service) {
         this.memberRepository = memberRepository;
+        this.s3Service = s3Service;
     }
 
     private Member findMember(Long memberId) {
@@ -34,5 +37,20 @@ public class MemberService {
         Member member = findMember(memberId);
 
         return MemberResponse.from(member);
+    }
+
+    @Transactional
+    public void uploadProfileImage(Long memberId, MultipartFile file) {
+        Member member = findMember(memberId);
+
+        String key = s3Service.uploadProfileImage(memberId, file);
+
+        member.updateProfileImageKey(key);
+    }
+
+    public String getProfileImageUrl(Long memberId) {
+        Member member = findMember(memberId);
+
+        return s3Service.generatePresignedUrl(member.getProfileImageKey());
     }
 }
---
+++ src/main/java/com/cloudmember/service/S3Service.java
@@ -0,0 +1,78 @@
+package com.cloudmember.service;
+
+import io.awspring.cloud.s3.S3Template;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
+import software.amazon.awssdk.services.s3.presigner.model.PresignedGetObjectRequest;
+
+import java.io.IOException;
+import java.time.Duration;
+import java.util.UUID;
+
+@Service
+public class S3Service {
+
+    private static final Duration PRESIGNED_URL_EXPIRATION = Duration.ofDays(7);
+
+    private final S3Presigner s3Presigner;
+    private final S3Client s3Client;
+
+    @Value("${spring.cloud.aws.s3.bucket}")
+    private String bucket;
+
+    public S3Service(S3Presigner s3Presigner, S3Client s3Client) {
+        this.s3Presigner = s3Presigner;
+        this.s3Client = s3Client;
+    }
+
+    public String uploadProfileImage(Long memberId, MultipartFile file) {
+        try {
+            String fileName = "uploads/" + UUID.randomUUID() + "_" + file.getOriginalFilename();
+
+            String key = "profiles/" + memberId + "/" + fileName;
+
+            PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                    .bucket(bucket)
+                    .key(key)
+                    .contentType(file.getContentType())
+                    .build();
+
+            s3Client.putObject(
+                    putObjectRequest,
+                    RequestBody.fromInputStream(
+                            file.getInputStream(),
+                            file.getSize()
+                    )
+            );
+
+            return key;
+
+        } catch (IOException e) {
+            // 적절한 커스텀 예외로 바꾸고, GlobalExceptionHandler로 핸들링 필요
+            throw new RuntimeException("파일 업로드 실패", e);
+        }
+    }
+
+    public String generatePresignedUrl(String key) {
+        GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+                .bucket(bucket)
+                .key(key)
+                .build();
+
+        GetObjectPresignRequest presignRequest = GetObjectPresignRequest.builder()
+                .signatureDuration(PRESIGNED_URL_EXPIRATION)
+                .getObjectRequest(getObjectRequest)
+                .build();
+
+        PresignedGetObjectRequest presignedGetObjectRequest = s3Presigner.presignGetObject(presignRequest);
+
+        return presignedGetObjectRequest.url().toString();
+    }
+}
